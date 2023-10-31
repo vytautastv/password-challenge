4b08f3c866db44af95488d1de5bd919521e1f044
vytautas <vytautastv98@gmail.com>
2023-10-31 11:45:13 +0000

    added javasript code that uses the provided arrays to prompt a user to choose the length, special charactersa and other features to be included in their password - this code follows the week 5 teachings for usage of arrays, functions, object

--- starter/script.js
+++ starter/script.js
@@ -114,5 +114,84 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Added an event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  // Prompt for password length
+  var length = parseInt(prompt("Enter the length of your password (between 8 and 128 characters):"));
+  
+  // Validate the length
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid number between 8 and 128.");
+    return getPasswordOptions(); // Ask again if invalid input
+  }
+
+  // Prompt for character types
+  var includeLower = confirm("Do you want to include lowercase characters?");
+  var includeUpper = confirm("Do you want to include uppercase characters?");
+  var includeNumeric = confirm("Do you want to include numeric characters?");
+  var includeSpecial = confirm("Do you want to include special characters?");
+  
+  // Validate at least one character type is selected
+  if (!includeLower && !includeUpper && !includeNumeric && !includeSpecial) {
+    alert("At least one character type must be selected.");
+    return getPasswordOptions(); // Ask again if no type selected
+  }
+
+  return {
+    length: length,
+    includeLower: includeLower,
+    includeUpper: includeUpper,
+    includeNumeric: includeNumeric,
+    includeSpecial: includeSpecial
+  };
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  var options = getPasswordOptions();
+
+  // Create a pool of characters based on user choices
+  var characterPool = [];
+  if (options.includeLower) {
+    characterPool = characterPool.concat(lowerCasedCharacters);
+  }
+  if (options.includeUpper) {
+    characterPool = characterPool.concat(upperCasedCharacters);
+  }
+  if (options.includeNumeric) {
+    characterPool = characterPool.concat(numericCharacters);
+  }
+  if (options.includeSpecial) {
+    characterPool = characterPool.concat(specialCharacters);
+  }
+
+  var password = "";
+  for (var i = 0; i < options.length; i++) {
+    password += getRandom(characterPool);
+  }
+
+  return password;
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
